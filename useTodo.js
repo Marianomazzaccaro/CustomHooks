bcb6b3ed6da6a117a782cca1939118cfe76f8631
mmazzaccaro <m.mazzaccaro@hotmail.com>
2022-10-20 17:45:28 -0300

    Primer Commit

---
+++ useTodo.js
@@ -0,0 +1,58 @@
+import { useReducer, useState,useEffect} from 'react';
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+
+const initialState = [];
+const init =() =>{
+    return JSON.parse(localStorage.getItem('todos')) || [];
+}
+
+export const useTodo = () =>{   
+
+    const [todos, dispatch] = useReducer(todoReducer, [],init)
+
+    useEffect(() => {
+        localStorage.setItem('todos',JSON.stringify(todos));
+        
+    }, [todos])
+    
+
+    const handleNewTodo=(todo)=>{
+
+        const action = {
+            type: '[TODO] Add Todo',
+            payload: todo,
+        }
+
+        dispatch(action);
+        // console.log({todo});
+    }
+
+    const handleDeleteTodo = (id) =>{
+
+        
+        dispatch ({
+            type: '[TODO] Remove Todo',
+            payload:id,
+
+        });
+    }
+    const handleToggleTodo = (id) =>{
+        // console.log(id)
+        
+        dispatch ({
+            type: '[TODO] Toggle Todo',
+            payload:id,
+
+        });
+    }
+
+    return {
+        todos,
+        todosCount: todos.length,
+        pendingTodosCount: todos.filter(todo=>!todo.done).length,
+        handleNewTodo,
+        handleDeleteTodo,
+        handleToggleTodo,
+    }
+}
